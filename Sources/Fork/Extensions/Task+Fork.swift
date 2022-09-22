8ac6110e04943d03492bfaa1089d4e265f254c1a
Zach <zmeriksen@icloud.com>
2022-09-21 18:06:20 -0600

    Feature/fork merging (#15)
    - Add Task.withCheckedCancellation
    - Add extra merged function to Fork for Void output
    - Refactor files to split core code vs extensions

---
+++ Sources/Fork/Extensions/Task+Fork.swift
@@ -0,0 +1,20 @@
+//
+//  Task+Fork.swift
+//  
+//
+//  Created by Leif on 9/21/22.
+//
+
+extension Task where Success == Never, Failure == Never {
+    /// Run the async throwing task while checking for cancellation before and after.
+    @discardableResult
+    public static func withCheckedCancellation<Output>(
+        task: () async throws -> Output
+    ) async throws -> Output {
+        try checkCancellation()
+        let output = try await task()
+        try checkCancellation()
+        
+        return output
+    }
+}
--- Sources/Fork/Fork.swift
+++ Sources/Fork/Fork.swift
@@ -21,24 +21,6 @@ public struct Fork<LeftOutput, RightOutput> {
     ///   - leftOutput: An `async` closure that uses `LeftInput` to return `LeftOutput`
     ///   - rightOutput: An `async` closure that uses `RightInput` to return `RightOutput`
     public init<Value, LeftInput, RightInput>(
-        value: Value,
-        leftInputMap: @escaping (Value) throws -> LeftInput,
-        rightInputMap: @escaping (Value) throws -> RightInput,
-        leftOutput: @escaping (LeftInput) async throws -> LeftOutput,
-        rightOutput: @escaping (RightInput) async throws -> RightOutput
-    ) {
-        left = { try await leftOutput(try leftInputMap(value)) }
-        right = { try await rightOutput(try rightInputMap(value)) }
-    }
-    
-    /// Create a ``Fork`` using a single `Value` that is mapped for the left and right output functions.
-    /// - Parameters:
-    ///   - value: The value to be passed into the map functions
-    ///   - leftInputMap: Maps the `Value` into `LeftInput`
-    ///   - rightInputMap: Maps the `Value` into `RightInput`
-    ///   - leftOutput: An `async` closure that uses `LeftInput` to return `LeftOutput`
-    ///   - rightOutput: An `async` closure that uses `RightInput` to return `RightOutput`
-    public init<Value, LeftInput, RightInput>(
         value: @escaping () async throws -> Value,
         leftInputMap: @escaping (Value) throws -> LeftInput,
         rightInputMap: @escaping (Value) throws -> RightInput,
@@ -47,6 +29,47 @@ public struct Fork<LeftOutput, RightOutput> {
     ) {
         left = { try await leftOutput(try leftInputMap(value())) }
         right = { try await rightOutput(try rightInputMap(value())) }
+    }
+    
+    /// Combine the `LeftOutput` and `RightOutput` into a single `Output`
+    ///
+    /// - Returns: An `async` closure that returns the `Output` of the Fork's left and right paths
+    public func merge<Output>(
+        using: @escaping (LeftOutput, RightOutput) async throws -> Output
+    ) -> () async throws -> Output {
+        {
+            try await Task.withCheckedCancellation {
+                async let leftOutput = try left()
+                async let rightOutput = try right()
+                
+                return try await using(leftOutput, rightOutput)
+            }
+        }
+    }
+}
+
+extension Fork {
+    /// Create a ``Fork`` using a single `Value` that is mapped for the left and right output functions.
+    /// - Parameters:
+    ///   - value: The value to be passed into the map functions
+    ///   - leftInputMap: Maps the `Value` into `LeftInput`
+    ///   - rightInputMap: Maps the `Value` into `RightInput`
+    ///   - leftOutput: An `async` closure that uses `LeftInput` to return `LeftOutput`
+    ///   - rightOutput: An `async` closure that uses `RightInput` to return `RightOutput`
+    public init<Value, LeftInput, RightInput>(
+        value: Value,
+        leftInputMap: @escaping (Value) throws -> LeftInput,
+        rightInputMap: @escaping (Value) throws -> RightInput,
+        leftOutput: @escaping (LeftInput) async throws -> LeftOutput,
+        rightOutput: @escaping (RightInput) async throws -> RightOutput
+    ) {
+        self.init(
+            value: { value },
+            leftInputMap: leftInputMap,
+            rightInputMap: rightInputMap,
+            leftOutput: leftOutput,
+            rightOutput: rightOutput
+        )
     }
     
     /// Create a ``Fork`` using a single `Value` that is passed into the left and right output functions.
@@ -104,25 +127,7 @@ public struct Fork<LeftOutput, RightOutput> {
         )
     }
     
-    /// Combine the `LeftOutput` and `RightOutput` into a single `Output`
-    ///
-    /// - Returns: An `async` closure that returns the `Output` of the Fork's left and right paths
-    public func merge<Output>(
-        using: @escaping (LeftOutput, RightOutput) async throws -> Output
-    ) -> () async throws -> Output {
-        {
-            try Task.checkCancellation()
-            
-            async let leftOutput = try left()
-            async let rightOutput = try right()
-            
-            try Task.checkCancellation()
-            
-            return try await using(leftOutput, rightOutput)
-        }
-    }
-    
-    /// Combine the `LeftOutput` and `RightOutput` into a single `Output`
+    /// Merge the ``Fork`` and combine the `LeftOutput` and `RightOutput` into a single `Output`
     ///
     /// - Returns: The `Output` of the Fork's left and right paths
     public func merged<Output>(
@@ -131,8 +136,18 @@ public struct Fork<LeftOutput, RightOutput> {
         try await merge(using: using)()
     }
     
-    /// Combine the `Void LeftOutput` and `Void RightOutput`
+    /// Merge the ``Fork`` and return Void when `LeftOutput` and `RightOutput` are both Void
     public func merged() async throws where LeftOutput == Void, RightOutput == Void {
         try await merged(using: { _, _ in () })
+    }
+    
+    /// Merge the ``Fork`` and return the `RightOutput` when `LeftOutput` is Void
+    public func merged() async throws -> RightOutput where LeftOutput == Void {
+        try await merged(using: { _, output in output })
+    }
+    
+    /// Merge the ``Fork`` and return the `LeftOutput` when `RightOutput` is Void
+    public func merged() async throws -> LeftOutput where RightOutput == Void {
+        try await merged(using: { output, _ in output })
     }
 }
