9f25f1e41a29261fc7e1b2259b262535cd197796
Zach <zmeriksen@icloud.com>
2024-09-18 23:37:12 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -20,11 +20,7 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "Fork",
-            swiftSettings: [
-                .enableExperimentalFeature("StrictConcurrency"),
-                .unsafeFlags(["-warnings-as-errors"])
-            ]
+            name: "Fork"
         ),
         .testTarget(
             name: "ForkTests",
