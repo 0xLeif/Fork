05cacf79d0b6aa771cb3f91accc5d3e2bb167a8c
Zach <zmeriksen@icloud.com>
2024-09-19 20:03:51 -0600

    Fix swift 6 concurrency warnings (#19)
    * Fix swift 6 concurrency warnings
    
    * Prefer using enableExperimentalFeature
    
    * Improve batch API
    
    * Add flag for warnings as errors
    
    * Update macOS.yml
    
    * Update Package.swift
    
    * Update windows.yml
    
    * Update ubuntu.yml
    
    * Update ubuntu.yml

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,18 +9,16 @@ let package = Package(
         .iOS(.v13),
         .watchOS(.v6),
         .macOS(.v10_15),
-        .tvOS(.v13)
+        .tvOS(.v13),
+        .visionOS(.v1)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Fork",
             targets: ["Fork"]
         )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Fork"
         ),
--- Sources/Fork/Extensions/Sequence+BatchedForkedArray.swift
+++ Sources/Fork/Extensions/Sequence+BatchedForkedArray.swift
@@ -1,9 +1,9 @@
-extension Sequence {
+extension Sequence where Element: Sendable {
     /// Create a ``BatchedForkedArray`` from the current `Sequence`
-    public func batchedFork<Output>(
+    public func fork<Output: Sendable>(
         batch: UInt,
-        filter: @escaping (Element) async throws -> Bool,
-        map: @escaping (Element) async throws -> Output
+        filter: @Sendable @escaping (Element) async throws -> Bool,
+        map: @Sendable @escaping (Element) async throws -> Output
     ) -> BatchedForkedArray<Element, Output> {
         BatchedForkedArray(
             Array(self),
@@ -14,59 +14,59 @@ extension Sequence {
     }
 
     /// Create a ``BatchedForkedArray`` from the current `Sequence`
-    public func batchedFork<Output>(
+    public func fork<Output: Sendable>(
         batch: UInt,
-        map: @escaping (Element) async throws -> Output
+        map: @Sendable @escaping (Element) async throws -> Output
     ) -> BatchedForkedArray<Element, Output> {
-        batchedFork(batch: batch, filter: { _ in true }, map: map)
+        fork(batch: batch, filter: { _ in true }, map: map)
     }
 
     /// Create a ``BatchedForkedArray`` from the current `Sequence` and get the Output Array
-    public func batchedForked<Output>(
+    public func forked<Output: Sendable>(
         batch: UInt,
-        filter: @escaping (Element) async throws -> Bool,
-        map: @escaping (Element) async throws -> Output
+        filter: @Sendable @escaping (Element) async throws -> Bool,
+        map: @Sendable @escaping (Element) async throws -> Output
     ) async throws -> [Output] {
         try await fork(filter: filter, map: map).output()
     }
 
     /// Create a ``BatchedForkedArray`` from the current `Sequence` and get the Output Array
-    public func batchedForked<Output>(
+    public func forked<Output: Sendable>(
         batch: UInt,
-        map: @escaping (Element) async throws -> Output
+        map: @Sendable @escaping (Element) async throws -> Output
     ) async throws -> [Output] {
-        try await batchedForked(batch: batch, filter: { _ in true }, map: map)
+        try await forked(batch: batch, filter: { _ in true }, map: map)
     }
 
     /// Returns an array containing the results of mapping the given closure over the sequence’s elements.
-    public func asyncBatchedMap<Output>(
+    public func asyncMap<Output: Sendable>(
         batch: UInt,
-        _ transform: @escaping (Element) async throws -> Output
+        _ transform: @Sendable @escaping (Element) async throws -> Output
     ) async throws -> [Output] {
-        try await batchedFork(batch: batch, map: transform).output()
+        try await fork(batch: batch, map: transform).output()
     }
 
     /// Returns an array containing the results, that aren't nil, of mapping the given closure over the sequence’s elements.
-    public func asyncBatchedCompactMap<Output>(
+    public func asyncCompactMap<Output: Sendable>(
         batch: UInt,
-        _ transform: @escaping (Element) async throws -> Output?
+        _ transform: @Sendable @escaping (Element) async throws -> Output?
     ) async throws -> [Output] {
-        try await batchedFork(batch: batch, map: transform).output().compactMap { $0 }
+        try await fork(batch: batch, map: transform).output().compactMap { $0 }
     }
 
     /// Returns an array containing only the true results from the given closure over the sequence’s elements.
-    public func asyncBatchedFilter(
+    public func asyncFilter(
         batch: UInt,
-        _ isIncluded: @escaping (Element) async throws -> Bool
+        _ isIncluded: @Sendable @escaping (Element) async throws -> Bool
     ) async throws -> [Element] {
-        try await batchedFork(batch: batch, filter: isIncluded, map: identity).output()
+        try await fork(batch: batch, filter: isIncluded, map: identity).output()
     }
 
     /// Calls the given closure for each of the elements in the Sequence. This function uses ``BatchedForkedArray`` and will be parallelized when possible.
-    public func asyncBatchedForEach(
+    public func asyncForEach(
         batch: UInt,
-        _ transform: @escaping (Element) async throws -> Void
+        _ transform: @Sendable @escaping (Element) async throws -> Void
     ) async throws {
-        _ = try await asyncBatchedMap(batch: batch, transform)
+        _ = try await asyncMap(batch: batch, transform)
     }
 }
